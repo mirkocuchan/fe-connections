46f6404400ba58697cf5a4b9c31c716039f2d797
mirkocuchan <mirko.cuchan01@gmail.com>
2026-09-08 18:19:30 +0000

    creating the tabs, chats, discover, profile

---
+++ src/app/(tabs)/profile.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function ProfileScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText type="title" style={styles.title}>
+        Welcome to Profile
+      </ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+  },
+});
